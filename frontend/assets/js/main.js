a6228b2d48ca0d4ca7bf3e8ce44cea8ace6444c9
israelSandrino <isandrino99@gmail.com>
2023-09-05 23:28:36 -0300

    Layout da calculadora criado

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,4 +1,9 @@
 function login() {
-    const labelIndex = window.open('/frontend/index.html', '_self')
-    labelIndex.focus();
+    const logPlataform = window.open('/frontend/index.html', '_self')
+    logPlataform.focus();
+}
+
+function calculadora() {
+    const openCalculadora = window.open('/frontend/calculadora.html', '_self')
+    openCalculadora.focus();
 }
